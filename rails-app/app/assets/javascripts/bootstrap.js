1188db6610cce80f1fa2e586704c093b6c60086a
Jari Bakken <jari.bakken@gmail.com>
2012-02-21 00:53:39 +0100

    Add Twitter bootstrap

---
+++ rails-app/app/assets/javascripts/bootstrap.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $(".alert-message").alert();
+  $(".tabs").button();
+  $(".carousel").carousel();
+  $(".collapse").collapse();
+  $(".dropdown-toggle").dropdown()
+  $(".modal").modal();
+  $("a[rel=popover]").popover();
+  $(".navbar").scrollspy();
+  $(".tab").tab "show";
+  $(".tooltip").tooltip();
+  $(".typeahead").typeahead();
+  $("a[rel=tooltip]").tooltip();
+});
+
+
